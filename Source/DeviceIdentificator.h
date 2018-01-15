e15731918a4657435696983c2f9d927475262cb0
Sebastián Varela <sebastian.varela@s3ba.net>
2018-01-15 17:40:57 +0100

    Add empty project with initial code of UIDeviceExtensions

---
+++ Source/DeviceIdentificator.h
@@ -0,0 +1,19 @@
+//
+//  DeviceIdentificator.h
+//  DeviceIdentificator
+//
+//  Created by Sebastián Varela on 15/1/18.
+//  Copyright © 2018 Sebastián Varela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeviceIdentificator.
+FOUNDATION_EXPORT double DeviceIdentificatorVersionNumber;
+
+//! Project version string for DeviceIdentificator.
+FOUNDATION_EXPORT const unsigned char DeviceIdentificatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceIdentificator/PublicHeader.h>
+
+
